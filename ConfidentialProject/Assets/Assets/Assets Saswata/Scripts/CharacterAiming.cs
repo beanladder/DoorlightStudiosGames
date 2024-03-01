0a252866a007fad4eba7d7ccd3536b5b316dc898
Saswata Bhattacharjee <saswatabhattacharjee.2004@gmail.com>
2024-03-02 00:04:52 +0530

    Added Cinemachine and AnimationRigging

---
+++ ConfidentialProject/Assets/Assets/Assets Saswata/Scripts/CharacterAiming.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharacterAiming : MonoBehaviour
+{
+
+    public Transform cameraLookAt;
+    public Cinemachine.AxisState xAxis;
+    public Cinemachine.AxisState yAxis;
+    Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void Update()
+    {
+        xAxis.Update(Time.deltaTime);
+        yAxis.Update(Time.deltaTime);
+        cameraLookAt.localEulerAngles = new Vector3(yAxis.Value, cameraLookAt.localEulerAngles.y, cameraLookAt.localEulerAngles.z);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
+
+        //float mouseX = Input.GetAxis("Mouse X");
+        //anim.SetFloat("MouseX", mouseX);
+    }
+}
+
+
+
